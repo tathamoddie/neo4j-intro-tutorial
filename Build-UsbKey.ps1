07cb0befc0c00a793803dda4337911e3f0831c06
Tatham Oddie <tatham@oddie.com.au>
2013-07-10 22:21:16 +1000

    Add PS script for building USB keys on Windows

---
+++ Build-UsbKey.ps1
@@ -0,0 +1,52 @@
+#requires -version 2.0 -runasadministrator
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [ValidatePattern({^[A-Z]:$})]
+    [string]
+    $TargetDrive,
+
+    [switch]$Force = $false
+)
+
+Set-StrictMode -Version Latest
+
+$ErrorActionPreference = "Stop"
+
+$PSScriptFilePath = (Get-Item $MyInvocation.MyCommand.Path).FullName
+$PSScriptRoot = Split-Path $PSScriptFilePath -Parent
+
+function Get-Drive {
+    param($deviceID)
+    $drive = Get-WmiObject "Win32_Volume WHERE DriveLetter='$deviceID'"
+    if (-not $drive) {
+        throw "Drive not found"
+    }
+    $drive
+}
+
+function Format-Drive {
+    param($drive, $label)
+    # http://msdn.microsoft.com/en-us/library/windows/desktop/aa390432(v=vs.85).aspx
+    $quick = $true
+    $compressed = $false
+    $null = $drive.Format("FAT32", $quick, 4096, $label, $compressed)
+}
+
+function Dismount-Drive {
+    param($drive)
+    # http://msdn.microsoft.com/en-us/library/windows/desktop/aa390368(v=vs.85).aspx
+    $force = $false
+    $permanent = $false
+    $null = $drive.Dismount($force, $permanent)
+}
+
+if (-not $Force -and
+    @(gci $TargetDrive).Length -ne 0) {
+    throw "Drive contains files but -Force not supplied; aborting"
+}
+
+$drive = Get-Drive $TargetDrive
+Format-Drive $drive Neo4j
+gci (Join-Path $PSScriptRoot .\USBKEY) | cpi -Destination D: -Recurse
+[System.Media.SystemSounds]::Asterisk.Play();
